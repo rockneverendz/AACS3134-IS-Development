fbad2ad92842c85365014574262f606e493b9b84
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-03-14 01:06:53 +0800

    Update ReloadService.java

--- src/java/service/ReloadService.java
+++ src/java/service/ReloadService.java
@@ -1,6 +1,6 @@
 package service;
 
-import entity.Meal;
+import entity.Reload;
 import javax.persistence.*;
 import java.util.List;
 
@@ -13,59 +13,19 @@ public class ReloadService {
         this.em = emfactory.createEntityManager();
     }
 
-    public void addMeal(Meal meal) throws RollbackException {
+    public void addReload(Reload reload) throws RollbackException {
         em.getTransaction().begin();
-        em.persist(meal);
+        em.persist(reload);
         em.getTransaction().commit();
     }
 
-    /**
-     * @param id ID of the meal which needs to be deleted
-     * @return true if successfully committed false if meal not found
-     * @throws RollbackException If commit fails
-     */
-    public boolean deleteMeal(int id) throws RollbackException {
-        Meal meal = findMealByID(id);
-        if (meal != null) {
-            em.getTransaction().begin();
-            em.remove(meal);
-            em.getTransaction().commit();
-            return true;
-        }
-        return false;
+    public Reload findReloadByID(int id) {
+        return (Reload) em.find(Reload.class, id);
     }
 
-    public Meal findMealByID(int id) {
-        return (Meal) em.find(Meal.class, id);
-    }
-
-    /**
-     * @param newMeal The modified meal
-     * @return true if successfully committed false if meal not found
-     * @throws RollbackException If commit fails
-     */
-    public boolean updateMeal(Meal newMeal) throws RollbackException {
-        Meal oldMeal = findMealByID(newMeal.getId());
-        if (oldMeal != null) {
-            em.getTransaction().begin();
-            oldMeal.setName(newMeal.getName());
-            oldMeal.setDescription(newMeal.getDescription());
-            oldMeal.setDay(newMeal.getDay());
-            oldMeal.setPrice(newMeal.getPrice());
-            oldMeal.setIngredient(newMeal.getIngredient());
-            oldMeal.setIngredientQty(newMeal.getIngredientQty());
-            oldMeal.setCalories(newMeal.getCalories());
-            oldMeal.setImage(newMeal.getImage());
-            oldMeal.setCategoryid(newMeal.getCategoryid());
-            em.getTransaction().commit();
-            return true;
-        }
-        return false;
-    }
-
-    public List<Meal> findAll() {
-        List MealList = em.createNamedQuery("Meal.findAll").getResultList();
-        return MealList;
+    public List<Reload> findAll() {
+        List ReloadList = em.createNamedQuery("Reload.findAll").getResultList();
+        return ReloadList;
     }
 
     public void close() {
