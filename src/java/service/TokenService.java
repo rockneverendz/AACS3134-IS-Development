8ecc3964f73517d476aa35b68024d8edf3a785ee
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-04-06 15:35:26 +0800

    Updated Token Generator

---
+++ src/java/service/TokenService.java
@@ -0,0 +1,50 @@
+package service;
+
+import entity.Token;
+import javax.persistence.*;
+import java.util.List;
+
+public class TokenService {
+
+    EntityManager em;
+
+    public TokenService() {
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("AACS3134-IS-DevelopmentPU");
+        this.em = emfactory.createEntityManager();
+    }
+
+    public void addToken(Token token) throws RollbackException {
+        em.getTransaction().begin();
+        em.persist(token);
+        em.getTransaction().commit();
+    }
+
+    public Token findTokenByID(int id) {
+        return (Token) em.find(Token.class, id);
+    }
+
+    /**
+     * @param newToken The modified token
+     * @return true if successfully committed false if token not found
+     * @throws RollbackException If commit fails
+     */
+    public boolean updateToken(Token newToken) throws RollbackException {
+        Token oldToken = findTokenByID(newToken.getTokenId());
+        if (oldToken != null) {
+            em.getTransaction().begin();
+            oldToken.setStutus(newToken.getStutus());
+            em.getTransaction().commit();
+            return true;
+        }
+        return false;
+    }
+
+    public List<Token> findAll() {
+        List TokenList = em.createNamedQuery("Token.findAll").getResultList();
+        return TokenList;
+    }
+
+    public void close() {
+        this.em.close();
+    }
+}
