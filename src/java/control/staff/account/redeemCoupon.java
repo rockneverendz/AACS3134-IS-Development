b5eaf7174512986fac1872a447166ba0b74b3651
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-04-07 18:59:37 +0800

    Update redeemCoupon.java
    Check if there's still active coupon in the ordermeal and if there's none, update the ordermeal to 'Completed'

--- src/java/control/staff/account/redeemCoupon.java
+++ src/java/control/staff/account/redeemCoupon.java
@@ -1,15 +1,19 @@
 package control.staff.account;
 
 import entity.Coupon;
+import entity.Orderlist;
+import entity.Ordermeal;
 import service.CouponService;
 import java.io.IOException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import javax.persistence.NoResultException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import service.OrderService;
 
 public class redeemCoupon extends HttpServlet {
 
@@ -28,41 +32,56 @@ public class redeemCoupon extends HttpServlet {
             //Initialization
             CouponService couponService = new CouponService();
             Coupon coupon = couponService.findCouponByID(couponID);
-            String active = "Active";
-            String redeemed = "Redeemed";
-            String expired = "Expired";
             String status = coupon.getStatus();
             Date redeemDate = coupon.getRedeemDate();
             String redeemDateString = dateFormat.format(coupon.getRedeemDate());
             System.out.println(status);
+
+            int i;
             //Check Status
-            if (status.equalsIgnoreCase(active)) {
-                //Check is today date
-                if (redeemDateString.equalsIgnoreCase(todayDateString)) {
-                    //Update Status and send redirect
-                    couponService.setStatusRedeem(couponID);
-                    response.sendRedirect("redeemCoupon.jsp?status=1");
-                } else if (redeemDate.before(todayDate)) {    //Check if coupon is expired
-                    //Update coupon status and send redirect
-                    couponService.setStatusExpried(couponID);
+            switch (status) {
+                case "Active":
+                    //Check is today date
+                    if (redeemDateString.equalsIgnoreCase(todayDateString)) {
+                        //Update Status and send redirect
+                        couponService.setStatusRedeem(couponID);
+                        response.sendRedirect("redeemCoupon.jsp?status=1");
+                    } else if (redeemDate.before(todayDate)) {    //Check if coupon is expired
+                        //Update coupon status and send redirect
+                        couponService.setStatusExpried(couponID);
+                        response.sendRedirect("redeemCoupon.jsp?status=E");
+                    } else {
+                        response.sendRedirect("redeemCoupon.jsp?status=D");  //Not valid date for redeem
+                    }
+                    break;
+                case "Redeemed":
+                    response.sendRedirect("redeemCoupon.jsp?status=R");
+                    break;
+                case "Expired":
                     response.sendRedirect("redeemCoupon.jsp?status=E");
-                } else {
-                    response.sendRedirect("redeemCoupon.jsp?status=D");  //Not valid date for redeem
-                }
-
-            } else if (status.equalsIgnoreCase(redeemed)) {
-                response.sendRedirect("redeemCoupon.jsp?status=R");
-
-            } else if (status.equalsIgnoreCase(expired)) {
-                response.sendRedirect("redeemCoupon.jsp?status=E");
+                    break;
+                default:
+                    break;
             }
-        } catch (NoResultException ex) {
-            response.sendRedirect("redeemCoupon.jsp?status=U");
-            System.out.println(ex.getMessage());
-        } catch (NumberFormatException ex) {
-            response.sendRedirect("redeemCoupon.jsp?status=U");
-            System.out.println(ex.getMessage());
-        } catch (NullPointerException ex) {
+
+            // Get ordermeal from database to ensure it's up to date. ( From coupon update above )
+            OrderService orderservice = new OrderService();
+            Ordermeal ordermeal = orderservice.findOrderByID(coupon.getOrderlistList().get(0).getOrdermeal().getOrderId());
+            List<Orderlist> list = ordermeal.getOrderlistList();
+
+            // Check if every coupon is redeem
+            for (i = 0; i < list.size(); i++) {
+                if (list.get(i).getCoupon().getStatus().equals("Active")) {
+                    i = list.size() + 1;
+                }
+            }
+
+            // If 'i' made it through the whole list
+            if (i == list.size()) {
+                ordermeal.setStatus("Completed");
+            }
+
+        } catch (NoResultException | NumberFormatException | NullPointerException ex) {
             response.sendRedirect("redeemCoupon.jsp?status=U");
             System.out.println(ex.getMessage());
         }
